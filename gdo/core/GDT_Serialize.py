486cf3fcc5d3217436a80c3a51b31774304d542e
gizmore <gizmore@wechall.net>
2024-04-16 00:51:20 +0200

    'text and serialize'

--- gdo/core/GDT_Serialize.py
+++ gdo/core/GDT_Serialize.py
@@ -1,3 +1,5 @@
+import pickle
+
 from gdo.core.GDT_Text import GDT_Text
 
 
@@ -5,4 +7,16 @@ class GDT_Serialize(GDT_Text):
 
     def __init__(self, name):
         super().__init__(name)
+        self.binary()
+        self.maxlen(65535)
+
+    def to_val(self, value) -> bytes:
+        return pickle.dumps(value)
+
+    def to_value(self, val: bytes):
+        return pickle.loads(val)
+
+    def validate(self, value):
+        return True
+
 
--- gdo/core/GDT_Text.py
+++ gdo/core/GDT_Text.py
@@ -2,6 +2,16 @@ from gdo.core.GDT_String import GDT_String
 
 
 class GDT_Text(GDT_String):
+    """
+    A longer chunk of text, using a different mysql column define
+    """
 
     def __init__(self, name):
         super().__init__(name)
+
+    def gdo_column_define(self) -> str:
+        return (f"{self._name} TEXT({self._maxlen}) "
+                f"CHARSET {self.gdo_column_define_charset()} COLLATE {self.gdo_column_define_collate()} "
+                f"{self.gdo_column_define_default()} "
+                f"{self.gdo_column_define_null()} ")
+
