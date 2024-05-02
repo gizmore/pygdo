52c979ac915a6a589235976aa7d361db5748faac
gizmore <gizmore@wechall.net>
2024-05-02 19:48:10 +0200

    'pretty status'

--- gdo_status.sh
+++ gdo_status.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 CORE="$(dirname "$0")"
 
 clear
-find . -maxdepth 3 -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && LANG=en_GB LC_ALL=en_GB git status && git submodule foreach git status" \;
+find . -maxdepth 3 -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && echo "--------------------------------" && pwd && LANG=en_GB LC_ALL=en_GB git status && git submodule foreach git status" \;
