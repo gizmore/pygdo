556fabe337924020387f874068cf1b24eb1317aa
gizmore <gizmore@wechall.net>
2024-05-04 01:33:22 +0200

    'fixes'

--- gdo/core/InstallUser.py
+++ gdo/core/InstallUser.py
@@ -1,3 +1,4 @@
+from gdo.base.Application import Application
 from gdo.core.GDO_Permission import GDO_Permission
 from gdo.core.GDO_Server import GDO_Server
 from gdo.core.GDO_User import GDO_User
@@ -26,10 +27,11 @@ class InstallUser:
     @classmethod
     def install_bash(cls):
         if not GDO_Server.get_by_connector('Bash'):
-            GDO_Server.blank({
+            Application.SERVER = GDO_Server.blank({
                 'serv_name': 'Bash',
                 'serv_connector': 'Bash',
             }).insert()
+
 
     @classmethod
     def install_web(cls):
