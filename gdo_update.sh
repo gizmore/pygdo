d74223b5dbc5aae4dbae8d2255b2e0cb3e00153f
gizmore <gizmore@wechall.net>
2024-05-04 01:27:15 +0200

    'fixes'

--- gdo_update.sh
+++ gdo_update.sh
@@ -27,15 +27,15 @@ echo "Updating all core modules and submodules in the phpgdo repo."
 find ./ -maxdepth 1 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && git submodule foreach git pull >> temp/git_pull_\$OUT && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> temp/git_pull_\$OUT && git submodule update --recursive --remote &>> temp/git_pull_\$OUT  ; cat temp/git_pull_\$OUT && rm temp/git_pull_\$OUT "
 
 echo "Updating all extension modules and submodules in $THREADS parallel threads."
-find ./GDO -maxdepth 2 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> ../../temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> ../../temp/git_pull_\$OUT && git submodule foreach git pull >> ../../temp/git_pull_\$OUT && git submodule update --recursive --remote &>> ../../temp/git_pull_\$OUT  ; cat ../../temp/git_pull_\$OUT && rm ../../temp/git_pull_\$OUT "
+find ./gdo -maxdepth 2 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> ../../temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> ../../temp/git_pull_\$OUT && git submodule foreach git pull >> ../../temp/git_pull_\$OUT && git submodule update --recursive --remote &>> ../../temp/git_pull_\$OUT  ; cat ../../temp/git_pull_\$OUT && rm ../../temp/git_pull_\$OUT "
 
 cd "$(dirname "$0")"
 
-echo "Triggering 'gdo_adm.sh confgrade'."
+echo "Triggering 'gdo_adm.sh configure'."
 bash gdo_adm.sh confgrade
 
-echo "Triggering 'gdo_adm.sh update'."
-bash gdo_adm.sh update
+#echo "Triggering 'gdo_adm.sh update'."
+#bash gdo_adm.sh update
 
-echo "Triggering 'gdo_yarn.sh'."
-bash gdo_yarn.sh
+#echo "Triggering 'gdo_yarn.sh'."
+#bash gdo_yarn.sh
