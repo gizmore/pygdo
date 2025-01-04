782c233bb68039642521a491504d3bc86258d700
gizmore <gizmore@wechall.net>
2025-01-04 22:59:47 +0100

    'csrf memory storage'

--- gdo/form/GDT_CSRF.py
+++ gdo/form/GDT_CSRF.py
@@ -1,10 +1,12 @@
 from gdo.base.Application import Application
+from gdo.core.GDO_User import GDO_User
 from gdo.core.GDT_Token import GDT_Token
 from gdo.form.GDT_Hidden import GDT_Hidden
 
 
 class GDT_CSRF(GDT_Hidden):
     TOKEN_LEN = 12
+    STORAGE = {}
 
     def __init__(self, name='csrf'):
         super().__init__(name)
@@ -19,20 +21,22 @@ class GDT_CSRF(GDT_Hidden):
 
     def generate_token(self) -> str:
         token = GDT_Token.random(self.TOKEN_LEN)
-        sess = Application.get_session()
-        tokens = sess.get('csrf', [])
-        tokens.append(token)
-        sess.set('csrf', tokens[-10:])
+        uid = GDO_User.current().get_id()
+        if not uid in self.STORAGE:
+            self.STORAGE[uid] = []
+        self.STORAGE[uid].append(token)
+        self.STORAGE[uid] = self.STORAGE[uid][-10:]
         return token
 
     def validate(self, val: str | None, value: any) -> bool:
         if Application.is_unit_test():
             return True
         sess = Application.get_session()
-        tokens = sess.get('csrf', [])
+        uid = GDO_User.current().get_id()
+        tokens = self.STORAGE[uid]
         if value not in tokens:
             return self.error('err_csrf')
         tokens.remove(value)
-        sess.set('csrf', tokens)
+        # sess.set('csrf', tokens)
         return True
 
