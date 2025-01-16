c07a22aa5cd7473fa7cc75e0b25e51964916bc96
gizmore <gizmore@wechall.net>
2025-01-17 00:36:18 +0100

    'revcount'

--- gdo_revcount.sh
+++ gdo_revcount.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-FILE="gdo/base/GDO_Module.py"
+PYTHON_FILE="gdo/base/GDO_Module.py"
 
-sed -i -E 's/(CORE_REV\s*=\s*"PyGDOv[0-9]+\.[0-9]+\.[0-9]+-r)([0-9]+)/echo "    \"\1$((\2 + 1))\""/e' "$FILE"
+sed -i -E 's/(CORE_REV\s*=\s*"PyGDOv[0-9]+\.[0-9]+\.[0-9]+-r)([0-9]+)"/echo "    \1$((\2 + 1))\""/e' "$PYTHON_FILE"
