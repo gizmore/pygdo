7531e33c4e1bfedade78c1d6176085ee4db97410
Christian Busch <cb@oncrea.de>
2025-01-18 21:13:17 +0100

    'gizmore mac setup'

--- gdo_gizmore.sh
+++ gdo_gizmore.sh
@@ -3,6 +3,7 @@
 bash gdo_adm.sh wipe -a
 bash gdo_adm.sh install irc,chatgpt4o,pm,markdown,blackjack,account,admin,contact,perf,math,login,register,bootstrap5,avatar
 bash gdo_adm.sh admin gizmore 11111111 gizmore@wechall.net
+bash gdo_adm.sh admin christianbusch 11111111 cb@wechall.net
 bash gdo_adm.sh admin --server 2 gizmore 11111111 gizmore@wechall.net
 pygdo \$add_server giz irc tcp://irc.giz.org:6667
 pygdo \$cc
