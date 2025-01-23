b37953b1def5228babd9056a89dcd88e583f2580
gizmore <gizmore@wechall.net>
2025-01-23 05:35:03 +0100

    'bump'

--- gdo/base/GDO_Module.py
+++ gdo/base/GDO_Module.py
@@ -22,7 +22,7 @@ from gdo.base.WithModuleConfig import WithModuleConfig
 
 class GDO_Module(WithModuleConfig, GDO):
     CORE_VERSION = Version("8.0.0")
-    CORE_REV = "PyGDOv8.0.0-r1209"
+    CORE_REV = "PyGDOv8.0.0-r1210"
 
     _priority: int
     _inited: bool
