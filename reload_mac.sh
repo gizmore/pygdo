844f8af0bf00c0e51496bff91c2be3aa19e80c1d
Christian Busch <cb@oncrea.de>
2025-02-06 07:27:41 +0100

    log output

--- reload_mac.sh
+++ reload_mac.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
+cat /opt/homebrew/var/log/unit/unit.log
+rm -rf /opt/homebrew/var/log/unit/unit.log
 rm -f temp/yappi*.log
 while pgrep unit >/dev/null; do kill -9 $(pgrep unit); sleep 0.5; done
 unitd
