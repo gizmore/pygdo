4c597ae2afd8f367d7c3e6cce87f0627b9cd8adf
gizmore <gizmore@wechall.net>
2025-03-02 11:48:46 +0100

    'jhv'

--- gdo/base/GDO_Module.py
+++ gdo/base/GDO_Module.py
@@ -21,7 +21,7 @@ from gdo.base.WithModuleConfig import WithModuleConfig
 
 class GDO_Module(WithModuleConfig, GDO):
     CORE_VERSION = Version("8.0.1")
-    CORE_REV = "PyGDOv8.0.1-r1268"
+    CORE_REV = "PyGDOv8.0.1-r1269"
 
     METHOD_CACHE = {}
 
