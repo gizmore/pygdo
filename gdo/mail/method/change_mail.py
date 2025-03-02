f51ee1ee12a4e5d3a6ba18c8ba567d04b398d295
Christian Busch <cb@oncrea.de>
2025-03-02 13:59:44 +0100

    sync

---
+++ gdo/mail/method/change_mail.py
@@ -0,0 +1,15 @@
+from gdo.form.GDT_Form import GDT_Form
+from gdo.form.MethodForm import MethodForm
+from gdo.mail.GDT_Email import GDT_Email
+
+
+class change_mail(MethodForm):
+
+    def gdo_create_form(self, form: GDT_Form) -> None:
+        form.add_field(
+            GDT_Email('new_email').not_null(),
+        )
+        super().gdo_create_form(form)
+
+    def form_submitted(self):
+        pass
--- gdo/user/GDT_Gender.py
+++ gdo/user/GDT_Gender.py
@@ -5,8 +5,18 @@ class GDT_Gender(GDT_Enum):
 
     def __init__(self, name):
         super().__init__(name)
+        self._simple = False
+
+    def simple(self, simple: bool = True):
+        self._simple = simple
+        return self
 
     def gdo_choices(self) -> dict:
+        if self._simple:
+            return {
+                'male': 'Male',
+                'female': 'Female',
+            }
         return {
             'male': 'Male',
             'female': 'Female',
