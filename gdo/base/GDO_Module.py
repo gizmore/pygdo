1a6c2391160285dad193f89972e1eb7e6b5e416c
gizmore <gizmore@wechall.net>
2025-03-07 06:36:27 +0100

    'shadowdogs'

--- gdo/base/GDO_Module.py
+++ gdo/base/GDO_Module.py
@@ -21,7 +21,7 @@ from gdo.base.WithModuleConfig import WithModuleConfig
 
 class GDO_Module(WithModuleConfig, GDO):
     CORE_VERSION = Version("8.0.1")
-    CORE_REV = "PyGDOv8.0.1-r1276"
+    CORE_REV = "PyGDOv8.0.1-r1277"
 
     METHOD_CACHE = {}
 
