0b9ffaf3a3464615c1321b019614aa5f8cd33211
gizmore <gizmore@wechall.net>
2025-03-12 08:08:06 +0100

    'ipc'

--- gdo/base/IPC.py
+++ gdo/base/IPC.py
@@ -1,2 +1,11 @@
 class IPC:
-    pass
+
+    @classmethod
+    def send_to_web(cls, event: str, key: str, val: dict[str, str]):
+      pass
+
+    @classmethod
+    def send_to_cli(cls, event: str, key: str, val: dict[str, str]):
+      pass
+
+    
