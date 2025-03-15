984b6e56263cbb76ff10150631f40e06bb511e09
gizmore <gizmore@wechall.net>
2025-03-15 19:22:56 +0100

    'do_as'

---
+++ gdo/core/method/do_as.py
@@ -0,0 +1,28 @@
+from gdo.base.Application import Application
+from gdo.base.GDT import GDT
+from gdo.base.Message import Message
+from gdo.base.Method import Method
+from gdo.core.GDT_RestOfText import GDT_RestOfText
+from gdo.core.GDT_User import GDT_User
+
+
+class do_as(Method):
+
+    def gdo_trigger(self) -> str:
+        return 'as'
+
+    def gdo_parameters(self) -> [GDT]:
+        return [
+            GDT_User('user').not_null(),
+            GDT_RestOfText('cmd').not_null(),
+        ]
+
+    def gdo_user_permission(self) -> str | None:
+        return 'admin'
+
+    async def gdo_execute(self) -> GDT:
+        user = self.param_value('user')
+        msg = Message(' '.join(self.param_val('cmd')), self._env_mode).env_copy(self).env_user(user, True)
+        await msg.execute()
+        # Application.MESSAGES.put(msg)
+        return self.empty()
