4d7924863a89f0f5533bbb1f0ec260f107a2f319
gizmore <gizmore@wechall.net>
2025-03-15 23:34:47 +0100

    'do_as'

--- gdo/core/method/do_as.py
+++ gdo/core/method/do_as.py
@@ -22,5 +22,5 @@ class do_as(Method):
 
     async def gdo_execute(self) -> GDT:
         user = self.param_value('user')
-        msg = Message(' '.join(self.param_val('cmd')), self._env_mode).env_copy(self).env_user(user, True)
-        return msg.execute()
+        await Message(' '.join(self.param_val('cmd')), self._env_mode).env_copy(self).env_user(user, True).execute()
+        return self.empty()
