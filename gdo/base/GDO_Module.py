04ba70af3e652af96d774a80219b2a4e931c5737
Christian Busch <cb@oncrea.de>
2025-04-16 08:25:20 +0200

    'req'

--- gdo/base/GDO_Module.py
+++ gdo/base/GDO_Module.py
@@ -21,7 +21,7 @@ from gdo.base.WithModuleConfig import WithModuleConfig
 
 class GDO_Module(WithModuleConfig, GDO):
     CORE_VERSION = Version("8.0.1")
-    CORE_REV = "PyGDOv8.0.1-r1327"
+    CORE_REV = "PyGDOv8.0.1-r1328"
 
     METHOD_CACHE = {}
 
