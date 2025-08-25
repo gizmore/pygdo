652188169cc76a1b7aaeffb22eb1be45b126eda9
gizmore <gizmore@wechall.net>
2025-08-25 13:18:18 +0200

    'sd'

--- gdo/base/GDO.py
+++ gdo/base/GDO.py
@@ -221,7 +221,15 @@ class GDO(WithName, WithBulk, GDT):
         return self.save()
 
     def increment(self, key: str, by: float|int) -> Self:
-        return self.save_val(key, str(self.gdo_value(key) + by))
+        gdt = self.column(key)
+        old = gdt.get_value()
+        new = old + by
+        if gdt._max is not None and new > gdt._max:
+            new = gdt._max
+        elif gdt._min is not None and new < gdt._min:
+            new = gdt._min
+        self.set_val(key, str(new))
+        return self
 
     @classmethod
     def get_name(cls):
