eae42834a261bba60e9162074060d2899d09cd29
gizmore <gizmore@wechall.net>
2025-10-01 11:35:26 +0200

    'sd'

--- gdo/base/GDO_Module.py
+++ gdo/base/GDO_Module.py
@@ -23,7 +23,7 @@ from gdo.base.WithModuleConfig import WithModuleConfig
 
 class GDO_Module(WithModuleConfig, GDO):
     CORE_VERSION = Version("8.0.2")
-    CORE_REV = "PyGDOv8.0.2-r1138"
+    CORE_REV = "PyGDOv8.0.2-r1139"
 
     METHOD_CACHE: dict[str,Type['Method']] = {}
 
