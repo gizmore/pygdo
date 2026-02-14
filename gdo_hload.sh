bbd664602888df741a5226d894ebc83ad08b21b1
gizmore <gizmore@wechall.net>
2026-02-15 00:38:07 +0100

    pygdo

--- gdo_hload.sh
+++ gdo_hload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-h2load -n10000 -c16 --h1 -p HTTP/1.1 "http://localhost"
-h2load -n1     -c1  --h1 -p HTTP/1.1 "http://localhost?__yappi=1"
+h2load -n10000 -c16 --h1 -p HTTP/1.1 "http://py.giz.org"
+h2load -n1     -c1  --h1 -p HTTP/1.1 "http://py.giz.org?__yappi=1"
