b543da3f39664f7599e765b6739b0211972a9654
Christian Busch <cb@oncrea.de>
2026-02-15 09:39:08 +0100

    speed

--- gdo_h2load_mac.sh
+++ gdo_h2load_mac.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-h2load -n10000 -c8 --h1 -p HTTP/1.1 "http://localhost:31337"
-h2load -n1     -c1 --h1 -p HTTP/1.1 "http://localhost:31337?__yappi=1"
+h2load -n1 --h1 -p HTTP/1.1 "http://localhost:31337" > /dev/null
+h2load -n10000 -c16 -t2 --h1 -p HTTP/1.1 "http://localhost:31337"
+h2load -n1     -c1 --h1 -p HTTP/1.1 "http://localhost:31337?__yappi=1" > /dev/null
