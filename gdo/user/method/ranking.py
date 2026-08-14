c79776ce5c865a8f4e5247861a3753c8fe60fdf3
Mira <mira@mira-gpt.org>
2026-08-14 02:39:56 +0200

    Clean up ranking method

--- gdo/user/method/ranking.py
+++ gdo/user/method/ranking.py
@@ -35,32 +35,10 @@ class ranking(MethodQueryTable):
         GDO_User.join_setting(query, 'level')
         if module_enabled('country'):
             GDO_User.join_setting(query, 'country_living', 'country')
-        # self.apply_search(query)
-        # self.apply_filters(query)
         return query
 
     def gdo_order_default(self):
         return 'level DESC'
-
-    # def apply_search(self, query):
-    #     if search := self.table_search_field().get_val():
-    #         search = GDO_User.escape(search)
-    #         query.where(f"(gdo_user.user_name LIKE '%{search}%' OR gdo_user.user_displayname LIKE '%{search}%')")
-    #
-    # def apply_filters(self, query):
-    #     filters = self.table_filter_field()
-    #     if name := filters.filter_value(GDT_UserName('user_name')):
-    #         query.where(f"gdo_user.user_name LIKE '%{GDO_User.escape(name)}%'")
-    #     if level := filters.filter_value(GDT_Level('level')):
-    #         if level.isdigit():
-    #             query.where(f"CAST(COALESCE(setting_level.uset_val, '0') AS UNSIGNED)={int(level)}")
-    #     if module_enabled('country'):
-    #         from gdo.country.GDT_Country import GDT_Country
-    #         if country := filters.filter_value(GDT_Country('country')):
-    #             query.where(f"setting_country_living.uset_val={GDO_User.quote(country)}")
-    #     if server := filters.filter_value(GDT_UInt('user_server')):
-    #         if server.isdigit():
-    #             query.where(f"gdo_user.user_server={int(server)}")
 
     def render_user_name(self, _field, user: GDO_User):
         return GDT_ProfileLink().with_avatar(module_enabled('avatar')).user(user).render(Mode.render_cell)
