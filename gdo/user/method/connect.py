b71a60e600d6ab5f815a4cadf33650547698af84
gizmore <gizmore@wechall.net>
2026-08-27 19:55:56 +0200

    lang

--- gdo/user/method/connect.py
+++ gdo/user/method/connect.py
@@ -24,8 +24,9 @@ class connect(MethodForm):
         master = self._env_user
         slave = self.param_value('slave')
         if slave.get_id() == master.get_id() or slave.get_effective_user().get_id() == master.get_id():
-            return self.err('err_user_already_linked')
+            return self.empty('This account is already linked to you.')
         if slave.get_linked_user():
-            return self.empty('err_user_different_link')
+            return self.empty('This account is already linked. Unlink it before creating a new request.')
         token = UserLinkToken.create(master, slave)
-        return self.msg('msg_user_link_request', (slave.render_name(), f'$user.approve {token}'))
+        return self.empty(
+            f'Ask {slave.render_name()} to send: $user.approve {token}')
